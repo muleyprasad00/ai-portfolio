7391aa9ca140a2f1ad1d6f6edbf1df00d6c5a6b3
Prasad Muley <muleyprasad00@gmail.com>
2026-06-27 00:01:22 +0530

    security agaent push 2

--- scripts/security-agent.js
+++ scripts/security-agent.js
@@ -1,1 +1,13 @@
-console.log("🚀 AI Security Agent Started");
+const fs = require("fs");
+
+// GitHub provides a JSON file with event details
+const event = JSON.parse(
+    fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8")
+);
+
+console.log("=== PR Information ===");
+console.log("PR Number:", event.pull_request.number);
+console.log("Title:", event.pull_request.title);
+console.log("Author:", event.pull_request.user.login);
+console.log("Base Branch:", event.pull_request.base.ref);
+console.log("Source Branch:", event.pull_request.head.ref);
